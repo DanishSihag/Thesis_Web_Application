6f2a87d7b83f83be772e797ef603a70d24785450
Danish Sihag <sihag.danny570@gmail.com>
2022-10-02 16:46:28 +0200

    Added a collapsible sidebar for user options

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,3 +1,13 @@
 function open_login(){
   window.open("file:///C:/Users/sihag/Documents/GitHub/Thesis_Web_Application/html_webpages/login_email.html","_top");
 }
+
+function expand_sidebar() {
+  document.getElementById("collapsible_options").style.width = "210px";
+  document.getElementById("collapsible_sidebar").style.marginLeft = "1075px";
+}
+
+function collapse_sidebar() {
+  document.getElementById("collapsible_options").style.width = "0px";
+  document.getElementById("collapsible_sidebar").style.marginLeft= "1150px";
+}
