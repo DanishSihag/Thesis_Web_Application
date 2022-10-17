c3afe7434a53c011184302269f86aa8bddcfdd13
Danish Sihag <sihag.danny570@gmail.com>
2022-10-17 17:28:39 +0200

    Remade membership benefits section and completed UI using bootstrap framework in GROMAK.html

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,13 +1,3 @@
 function open_login(){
   window.open("file:///C:/Users/sihag/Documents/GitHub/Thesis_Web_Application/html_webpages/login_email.html","_top");
 }
-
-function expand_sidebar() {
-  document.getElementById("collapsible_options").style.width = "13.125rem";
-  document.getElementById("collapsible_sidebar").style.marginLeft = "67.188rem";
-}
-
-function collapse_sidebar() {
-  document.getElementById("collapsible_options").style.width = "0rem";
-  document.getElementById("collapsible_sidebar").style.marginLeft= "71.875rem";
-}
